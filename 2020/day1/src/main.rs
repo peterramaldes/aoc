5ba7c64ac50a860b290f14ef46598bbcd5fd5f3e
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-05-25 19:57:18 +0000

    2020, Day 1

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+const OPEN: char = '(';
+const CLOSED: char = ')';
+
 fn main() {
-    println!("Hello, world!");
+    // Read the file
+    let mut file = File::open("input.txt").unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+
+
+    let mut result = 0;
+    for c in input.chars() {
+       if c == OPEN { result += 1; }
+       if c == CLOSED { result -= 1; }
+    }
+
+    println!("{}", result);
 }
+
